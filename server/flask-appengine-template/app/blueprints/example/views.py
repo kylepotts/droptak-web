464215f7c9db8cb1a2d3ceac7a9ab34214738bd4
nick <nickstanish@gmail.com>
2014-01-24 22:08:04 -0500

    renamed obnoxious directory in server

--- server/flask-appengine-template/app/blueprints/example/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-
-bp = Blueprint('example', __name__, url_prefix = '/example')
-
-
-@bp.route('/')
-def home():
-    return render_template('example/home.html')
--- server/flask-appengine-template/app/utils/path.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-
-def fix_path(root): 
-    sys.path.insert(0, os.path.join(root, 'lib'))
