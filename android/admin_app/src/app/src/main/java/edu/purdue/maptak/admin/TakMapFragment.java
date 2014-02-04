ace277552da879cbc36f612ae2e211edb4af2be8
Michael Hockerman <mike@hockerman.com>
2014-02-04 15:01:44 -0500

    Added awesome new action bar theme to the app. Nice deep red with white text.

--- android/admin_app/src/app/src/main/java/edu/purdue/maptak/admin/TakMapFragment.java
+++ android/admin_app/src/app/src/main/java/edu/purdue/maptak/admin/TakMapFragment.java
@@ -2,7 +2,6 @@ package edu.purdue.maptak.admin;
 
 import android.content.Context;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 
