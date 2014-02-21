1a5b439fcbbb7d79769d3bd13918836c3fefbcdb
Kyle Potts <kyle@kptechblog.com>
2014-02-21 15:57:33 -0500

    Added storing login info on NDB

--- server/flaskapp/app/static/js/login.js
+++ server/flaskapp/app/static/js/login.js
@@ -1,11 +1,43 @@
-$(document).ready(function(){
-  $(document).mousemove(function(e){
-     TweenLite.to($('body'), 
-        .5, 
-        { css: 
-            {
-                'background-position':parseInt(event.pageX/8) + "px "+parseInt(event.pageY/12)+"px, "+parseInt(event.pageX/15)+"px "+parseInt(event.pageY/15)+"px, "+parseInt(event.pageX/30)+"px "+parseInt(event.pageY/30)+"px"
-            }
-        });
-  });
-});ß
+      (function() {
+       var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+       po.src = 'https://apis.google.com/js/client:plusone.js';
+       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+     })();
+
+
+function loginFinishedCallBack(authResult){
+  if (authResult) {
+    if (authResult['error'] == undefined){
+        document.getElementById('signinButton').setAttribute('style', 'display: none'); // Hide the sign-in button after successfully signing in the user.
+        gapi.client.load('plus','v1', getInfo);  // Trigger request to get the email address.
+      } else {
+        console.log('An error occurred');
+      }
+  } else {
+      console.log('Empty authResult');  // Something went wrong
+    }
+  }
+
+
+function getInfo(){
+ var request = gapi.client.plus.people.get({ 'userId': 'me'});
+ request.execute(getProfileCallBack);
+}
+
+
+function getProfileCallBack(obj){
+  profile = obj;
+  console.log(profile)
+  email = obj['emails'].filter(function(v) {
+        return v.type === 'account'; // Filter out the primary email
+    })[0].value;
+  name = obj['displayName'];
+  console.log("Email: " + email);
+  console.log("Name: "+ name);
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("POST","http://mapitapps.appspot.com/login?name="+name+"&email="+email,true);
+  xmlhttp.send()
+}
+
+
+
