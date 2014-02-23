d023a0b318e0f8b80d6184190fbd5202cfb2bf77
Kyle Potts <kyle@kptechblog.com>
2014-02-23 12:57:59 -0500

    Made base.html a default template, changed index.html to extends base, started working on map.html

--- server/flaskapp/app/static/js/map.js
+++ server/flaskapp/app/static/js/map.js
@@ -1,12 +1,3 @@
-var map;
-function initialize() {
-  var mapOptions = {
-    zoom: 16,
-    center: new google.maps.LatLng(40.4249, -86.9162)
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+var $ = jQuery.noConflict(); 
+console.log($)
+$(document).ready(function()  { $('#myCarousel').carousel({ interval: 3000, cycle: true }); });
