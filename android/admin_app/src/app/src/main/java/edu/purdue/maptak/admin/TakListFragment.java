6447d9e4989da68ac6460376f1153d2547ac7936
Travis Coria <coria@purdue.edu>
2014-03-05 22:38:16 +0000

    TakListFragment should properly display current taks.

--- android/admin_app/src/app/src/main/java/edu/purdue/maptak/admin/TakListFragment.java
+++ android/admin_app/src/app/src/main/java/edu/purdue/maptak/admin/TakListFragment.java
@@ -24,16 +24,15 @@ public class TakListFragment extends ListFragment {
 
     @Override public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        //MapTakDB db = new MapTakDB();
-        //List<TakObject> taks = db.getTaks(MainActivity.currentlySelectedMap);
+        MapTakDB db = new MapTakDB(getActivity());
+        List<TakObject> takObjects = db.getTaks(MainActivity.currentlySelectedMap);
 
         //Placeholder dummy data
         List<String> taks = new LinkedList<String>();
-        taks.add(DummyData.createDummyTakObject().getLabel());
-        taks.add(DummyData.createDummyTakObject().getLabel());
-        taks.add(DummyData.createDummyTakObject().getLabel());
-        taks.add(DummyData.createDummyTakObject().getLabel());
 
+        while(!takObjects.isEmpty()){
+            taks.add(takObjects.remove(0).getLabel());
+        }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, taks);
         setListAdapter(adapter);
