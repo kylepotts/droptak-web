56e369eef46f2496db54cec92fb4d73c71bf7d28
Kyle Potts <kyle@kptechblog.com>
2014-04-29 11:19:36 -0400

    fix logout

--- server/flaskapp/app/static/js/login.js
+++ server/flaskapp/app/static/js/login.js
@@ -37,6 +37,12 @@ function loginFinishedCallBack(authResult){
       //console.log(authResult)
       authRes = authResult
     // Hide the sign-in button now that the user is authorized, for example:
+    loggedIn = getCookie("loggedIn");
+    console.log("loggedIn="+loggedIn)
+    if(loggedIn == true){
+      console.log("Already loggedIn")
+      return
+    }
     $('#signinButton').attr('style', 'display: none');
     gapi.client.load('plus','v1', getInfo);  // Trigger request to get the email address.
 
@@ -128,5 +134,44 @@ function getProfileCallBack(obj){
     }
   });
   $('#myModal').modal('hide');
+  setCookie("loggedIn",true,100);
+}
+
+
+function setCookie(cname,cvalue,exdays)
+{
+var d = new Date();
+d.setTime(d.getTime()+(exdays*24*60*60*1000));
+var expires = "expires="+d.toGMTString();
+document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname)
+{
+var name = cname + "=";
+var ca = document.cookie.split(';');
+for(var i=0; i<ca.length; i++) 
+  {
+  var c = ca[i].trim();
+  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+}
+return "";
+}
+
+function checkCookie()
+{
+var user=getCookie("username");
+if (user!="")
+  {
+  alert("Welcome again " + user);
+  }
+else 
+  {
+  user = prompt("Please enter your name:","");
+  if (user!="" && user!=null)
+    {
+    setCookie("username",user,365);
+    }
+  }
 }
 
